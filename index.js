4d207d9a86ad41f245a119c84d303e68fdd9d654
coffee <39930080+coffee522jelly-development@users.noreply.github.com>
2021-12-12 18:01:02 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
+// APIのURL
 const url = 'https://think-free.microcms.io/api/v1/news/_Ej9SD_IN';
 
-//操作したいHTML領域を取得
-const news = document.getElementById('news');
-const main = document.getElementById('main');
-
-//APIからJSONデータを取得する
+// jsonの読込設定
 fetch(url, {
-  method: "GET",
-  withCredentials: true,
   headers: {
-    "X-MICROCMS-API-KEY :": "09fdf236-e540-414d-927a-70f425e43a3e",
+    "X-MICROCMS-API-KEY": "09fdf236-e540-414d-927a-70f425e43a3e",   // 個人のAPIキー
     "Content-Type": "application/json"
   }
 })
-  .then(resp => resp.json())
-  .then(function(data) {
-    console.log(data);
+  .then(res => res.json())
+  .then(json => {
+    // jsonパース部分
+    document.getElementById("news").innerHTML = json.news;
+    document.getElementById("createdAt").innerHTML = json.createdAt;
   })
   .catch(function(error) {
     console.log(error);
   });
-//JSONデータを引数に受け取ってDOM操作を行う関数を作成
-function GetMainContents(jsonObj){
- const data = jsonObj.results[0]
- news.textContent = data.news;
- main.textContent = data.main;
-}

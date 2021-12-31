63357238c59f945c0d9ff6af56581b9b42ed1b9d
coffee <coffeejellydev@gmail.com>
2022-01-01 03:21:20 +0900

    ローディングアニメーションの作成

--- js/calendar.js
+++ js/calendar.js
@@ -127,6 +127,7 @@ function get_month_calendar(year, month) {
     var firstDate = new Date(year, (month - 1), 1); // 指定した年月の初日の情報
     var lastDay = new Date(year, (firstDate.getMonth() + 1), 0).getDate(); // 指定した年月の末日
     var weekday = firstDate.getDay(); // 指定した年月の初日の曜日
+    var today = firstDate.getDay();
  
     var calendarData = []; // カレンダーの情報を格納
     var weekdayCount = weekday; // 曜日のカウント用
@@ -135,6 +136,7 @@ function get_month_calendar(year, month) {
             day: i + 1,
             weekday: weekdayCount
         }
+
         // 曜日のカウントが6(土曜日)まできたら0(日曜日)に戻す
         if(weekdayCount >= 6) {
             weekdayCount = 0;

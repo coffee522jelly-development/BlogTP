ec64d77d7d5b4364e79ac01935e477a6ef568d37
coffee <coffeejellydev@gmail.com>
2022-01-18 22:22:18 +0900

    綴り間違い

--- js/Google.js
+++ js/Google.js
@@ -1,5 +1,7 @@
+'use strict'
+
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'G-K02YHKRR6N');
+gtag('config', 'G-K02YHKRR6N'); 
